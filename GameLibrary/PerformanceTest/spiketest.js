085d13de7c1ff9e4289ac1e55b020b0e3bb3e2ab
fewture <113719482+m4gn3s1um@users.noreply.github.com>
2024-05-30 17:54:03 +0200

    Test and prod

---
+++ GameLibrary/PerformanceTest/spiketest.js
@@ -0,0 +1,14 @@
+﻿import http from 'k6/http'
+import {sleep, check} from 'k6';
+
+export const options = {
+    stages: [
+        {duration: '30s', target: 100},
+        {duration: '2m', target: 100},
+        {duration: '30s', target: 0},
+    ]
+};
+
+export default () => {
+    const res = http.get('http://localhost:5031/swagger/index.html')
+}
